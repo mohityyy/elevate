dd89765280f214f05919fddb66162b5eee33fcdf
mohityyy <mohityadav75539@gmail.com>
2021-04-04 13:47:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from './components/Header'
+import Frontpage from './components/Frontpage'
+import Main from './components/main/Main'
+import Footer from './components/Footer/Footer'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      
+<Frontpage/>
+<Main/>
+ <Footer/>
+  
+
+ 
+ 
+    </div>
+  );
+}
+
+export default App;
